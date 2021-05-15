3883324ffda2541ad17f0169f0a57f09088d846c
Daniel Hortencio <hortencio.daniel@gmail.com>
2021-05-15 14:39:26 -0400

    Criando input com busca de palavras chave

--- src/pages/teste.tsx
+++ src/pages/teste.tsx
@@ -3,10 +3,11 @@ import { api } from "services/api";
 
 export default function Test() {
   const [results, setResults] = useState<any>();
+  const [searchKeyWords, setSearchKeyWords] = useState<string>("");
 
   async function getBooks() {
     return await api.get(
-      `/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.NEXT_PUBLIC_API_KEY}`
+      `/v1/volumes?q=${searchKeyWords}&key=${process.env.NEXT_PUBLIC_API_KEY}`
     );
   }
 
@@ -16,14 +17,22 @@ export default function Test() {
         setResults(data.items);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [searchKeyWords]);
 
   return (
     <div>
-      <h1>Página de teste</h1>
+      <h1>Faça uma pesquisa:</h1>
+
+      <div style={{ border: "solid 1px red" }}>
+        <input
+          type="text"
+          onChange={(e) => setSearchKeyWords(e.target.value)}
+        />
+      </div>
+
       <h2>Resultados da pesquisa:</h2>
 
-      <ul>
+      <ul style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)" }}>
         {results &&
           results.map((book) => (
             <li
@@ -39,11 +48,14 @@ export default function Test() {
                 <img
                   src={book.volumeInfo.imageLinks?.thumbnail || ""}
                   alt="Imagem do produto"
+                  width={100}
+                  height={100}
+                  style={{ objectFit: "contain", border: "solid 1px red" }}
                 />
               )}
               <h3>{book.volumeInfo.title}</h3>
               <p>Descrição: {book.volumeInfo.description}</p>
-              <p>Data: {book.volumeInfo.publishedDate}</p>
+              <small>Data: {book.volumeInfo.publishedDate}</small>
             </li>
           ))}
       </ul>
